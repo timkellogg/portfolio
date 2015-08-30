68831e67a243b9c10b1c4e10517c30165302a6f0
timkellogg <tim.kellogg@gmail.com>
2015-08-29 21:26:57 -0700

    support basic caching for /public

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'bundler/setup'
 Bundler.require :default
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+before do
+  cache_control :public, :must_revalidate, :max_age => 60
+end
+
 get '/' do 
   erb :index
 end
@@ -9,3 +13,4 @@ end
 get '/*' do 
 	redirect '/'
 end
+
